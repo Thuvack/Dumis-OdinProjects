2bda0a6008044be462823e3570edbb41bc52b491
Thuvack <ndubaneds@gmail.com>
2022-03-09 21:57:54 +0200

    Completed exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayArg,...remArg) {
 
+    let result;
+    remArgArray = [];
+
+    for (let i = 0; i < remArg.length; i++ ) {
+
+        let j;
+
+        j = arrayArg.indexOf(remArg[i]);
+        
+        if (j == -1) {
+
+            console.log(remArg[i]+" is not in the array. Skipping");
+
+        } else {
+
+        arrayArg.splice(arrayArg.indexOf(remArg[i]), 1);
+
+        }
+    }
+
+    return arrayArg;
 };
 
 // Do not edit below this line
