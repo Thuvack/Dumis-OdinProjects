56115957bdaf882e6fed7818f6bce21d577af5ff
Thuvack <ndubaneds@gmail.com>
2022-03-09 18:30:04 +0200

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function (strArg) {
 
+    let newStr;
+    const strArray = [];
+    const strLen = strArg.length;
+
+    for (let i = 0; i <= strLen; i++){
+        strArray.push(strArg.charAt(strLen - i));
+    }
+
+    strArray.shift();
+
+    newStr = strArray.join("");
+
+    return newStr;
 };
 
 // Do not edit below this line
