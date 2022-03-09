cb31a8fb8120e2e03c2d4c68f17557c82277a5b5
Thuvack <ndubaneds@gmail.com>
2022-03-09 17:37:53 +0200

    Completed exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(strArg,numRepeat) {
+    
+    const orinStr = strArg;
 
+    if (numRepeat > 0) {
+
+        for (let i=1; i < numRepeat; i++) {
+            strArg = strArg + orinStr;
+        }
+    } else if (numRepeat == 0){
+        strArg = "";
+    } else {
+        strArg = "ERROR";
+    }
+    return strArg;
 };
 
 // Do not edit below this line
