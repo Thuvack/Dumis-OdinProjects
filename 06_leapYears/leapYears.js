3e04c37da9670dc039e5fc0212f35ee62a1defaa
Thuvack <ndubaneds@gmail.com>
2022-03-10 11:27:33 +0200

    Completed exercise #6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(argYear) {
 
+    let leapYear;
+        if (argYear%100 == 0 && argYear % 400 != 0) {
+            // Check year is century but not a leap year
+            leapYear = false;
+
+        }else if(argYear % 400 == 0 || argYear % 4 == 0) {
+            /// Check year is a century leap year or a simple leap year
+            leapYear = true;
+
+        } else {
+            // Then its not a leap year
+            leapYear = false;
+
+        }
+
+    return leapYear;
 };
 
 // Do not edit below this line
