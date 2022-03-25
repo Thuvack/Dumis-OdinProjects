a22db764370ea1c2d1376fc752c99771ea3959f7
Thuvack <ndubaneds@gmail.com>
2022-03-25 16:23:53 +0200

    Add draw function toggle
    Draw function added with toggle on click. This method works but
    Needs fixing to simply turn off event listeners on click.

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,28 +1,66 @@
 
 let myPad = document.querySelector(".sketchPad");
+let cellArray = [];
+let clickTracker = 0;
 let padSize = 100 * 100;
 
-for(let i = 0; i < padSize; i++) {
-    let div_name = "Cell_"+i;
-    let div = document.createElement(div_name);
-    div.classList.add("sketchPadCell");
+function sketchPadInit () {
 
-    // Add event listener to change background
-    div.addEventListener("mouseleave", function() {
-        div.style.background = "black";
-    })
+    for (let i = 0; i < padSize; i++) {
+        let div_name = "Cell_"+i;
+        let div = document.createElement(div_name);
+    
+        // Add class to new div
+        div.classList.add("sketchPadCell");
+    
+        // Add div name in an array
+        cellArray.push(div);
 
-    // Add event listener to switch on and off an event listener on click
+        // Append div in DOM
+        myPad.append(div);
+    }
 
-    myPad.append(div);
+    console.log("sketchpad Initialised");
 }
 
-// Add initialising function
+
+// Function to enable or disable drawing on mouseleave event
+
+function drawToggle () {
+
+    if (clickTracker == 0) {
+
+        for (let i = 0; i < padSize; i++) {
+            
+            cellArray[i].addEventListener("mouseleave", function() {
+            cellArray[i].style.background = "black";
+            })
+        
+        }
+        clickTracker = 1;
+
+    } else {
+
+        // TO FIX
+        // This function should remove previously added event listeners
+
+        for (let i = 0; i < padSize; i++) {
+            
+            cellArray[i].addEventListener("mouseleave", function() {
+            cellArray[i].style.background = "burlywood";
+            })
+        
+        }
+
+        clickTracker = 0;
+    }
+
+    console.log(clickTracker);
+}
+
+// Initialise sketchpad
+sketchPadInit();
 
 
-
-// Add event listeners for each div
-
-
-
-// Add sketchpad reset function
+// Add onclick event listener
+myPad.addEventListener("click", drawToggle);
