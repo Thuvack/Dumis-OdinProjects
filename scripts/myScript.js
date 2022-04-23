39a3dd4b0c16244f1823981fb7712e2046173bf0
Thuvack <ndubaneds@gmail.com>
2022-04-23 16:27:50 +0200

    Resolves #16

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,4 +1,6 @@
-let numKeyBtn = document.getElementsByClassName('calKeyNumWrapper')[0];
+// This is an implementation of an RPN scientific calculator
+
+// Screen display elements
 var output = document.getElementById("scrTopLeft");
 let currNum;
 
@@ -7,6 +9,14 @@ yStackDisp = document.getElementById("yStack");
 zStackDisp = document.getElementById("zStack");
 wStackDisp = document.getElementById("wStack");
 
+funcDisp1 = document.getElementById("scrBottomLeft");
+funcDisp2 = document.getElementById("scrBottomRight");
+
+// Define numeric button container
+let numKeyBtn = document.getElementsByClassName('calKeyNumWrapper')[0];
+
+// Operator buttons
+let operandTwoKeyBtn = document.getElementsByClassName('calKeyOp2Wrapper')[0];
 
 equalBtn = document.getElementById("equalKey");
 
@@ -14,11 +24,24 @@ enterBtn = document.getElementById("ENTER");
 
 clearBtn = document.getElementById("onK");
 
-let operandTwoKeyBtn = document.getElementsByClassName('calKeyOp2Wrapper')[0];
+shiftBtn = document.getElementById("shiftK");
+
+modeBtn = document.getElementById("stdMode");
+
+// Scientific Operator buttons
+
+
+
+
+// Calculator flags
+let numCaptFlag = "Ready";
+let shiftFlag = "Off";
+
 
 
 let calSTack = [];
 calSTack[0] = "";
+calSTack.pop();
 
 let inpSTack = [];
 inpSTack[0] = "";
@@ -29,29 +52,46 @@ let calMode = "RPN"
 
 function initCalc () {
 
-    if (calMode == "ALG") {
+    if (shiftFlag == "On") {
+
+        calMode = "ALG";
 
         enterBtn.style.display = "none";
+        equalBtn.style.display = "flex";
+
+        shiftFlag = "Off";
 
     } else {
 
+        calMode = "RPN"
+
         equalBtn.style.display = "none";
+        enterBtn.style.display = "flex";
 
     }
     
+    clearStack();
+
+    console.log(calMode)
 
 }
 
 function dispSTack () {
 
+    // Display the latest 4 numbers in the stack
     let ArrLen = calSTack.length;
 
-    console.log(ArrLen);
+    if (ArrLen == 1) {
 
-    if (ArrLen == 2) {
+        xStackDisp.innerHTML = calSTack[0];
+        yStackDisp.innerHTML = " ";
+        zStackDisp.innerHTML = " ";
+        wStackDisp.innerHTML = " ";
+
+    } else if (ArrLen == 2) {
 
         xStackDisp.innerHTML = calSTack[1];
-        yStackDisp.innerHTML = " ";
+        yStackDisp.innerHTML = calSTack[0];
         zStackDisp.innerHTML = " ";
         wStackDisp.innerHTML = " ";
 
@@ -59,17 +99,10 @@ function dispSTack () {
 
         xStackDisp.innerHTML = calSTack[2];
         yStackDisp.innerHTML = calSTack[1];
-        zStackDisp.innerHTML = " ";
+        zStackDisp.innerHTML = calSTack[0];
         wStackDisp.innerHTML = " ";
 
-    } else if (ArrLen == 4) {
-
-        xStackDisp.innerHTML = calSTack[3];
-        yStackDisp.innerHTML = calSTack[2];
-        zStackDisp.innerHTML = calSTack[1];
-        wStackDisp.innerHTML = " ";
-
-    } else if (ArrLen >= 5) {
+    } else if (ArrLen >= 4) {
 
         xStackDisp.innerHTML = calSTack[ArrLen-1];
         yStackDisp.innerHTML = calSTack[ArrLen-2];
@@ -78,6 +111,7 @@ function dispSTack () {
 
     } else {
 
+        // Do nothing
 
     }
 
@@ -86,20 +120,28 @@ function dispSTack () {
 
 function pushStack () {
 
-    if (calSTack.length <= 1) {
+    // Check if user has input a number
+    if (numCaptFlag == "Done") {
 
+        // Push captured number into stack
         calSTack.push(currNum);
 
-    } else if (calSTack.length >= 2) {
+        // Update stack display
+        dispSTack ();
 
-        //calSTack.push(currNum);
+        // Reset input stack
+        inpSTack = [];
+        inpSTack[0] = "";
+
+        // Unset number capture flag
+        numCaptFlag = "Ready"
+
+    } else {
+        
+        // DO nothing
 
     }
     
-    dispSTack ();
-
-    inpSTack = [];
-    inpSTack[0] = "";
 }
 
 
@@ -200,31 +242,43 @@ function captUserInput (event) {
 
     }
 
-    console.log(inpSTack);
-    
-    if (calSTack.length <= 1) {
-
-        //calSTack.push(currNum);
-
-    } else if (calSTack.length >= 2) {
-
-        calSTack.push(currNum);
-
-    }
-    
-
+    // Display captured number
     output.innerHTML = currNum;
+
+    // Set number capture flag to complete
+    numCaptFlag = "Done";
+
+    // Display the stack
+    dispSTack ();
+    
 }
 
 
 function operate (event) {
 
+    // Check if user is done entering number 
+    if (numCaptFlag == "Done") {
+
+        // Push number that has been captured into stack
+        calSTack.push(output.innerHTML);
+
+        // Unset number capture flag
+        numCaptFlag = "Ready";
+
+    } else {
+
+        // Operate numbers in the existing stack
+    }
+    
+
     // Get tagname of nummber key div that fired the event
     let opKey = event.target.id;
 
+    // Get numbers to be operated on from the stack
     let numRight = calSTack.pop();
     let numLeft = calSTack.pop();
 
+    // Perform operations
     if (opKey == "divideK") {
 
         let numDivident = numLeft/numRight;
@@ -264,6 +318,7 @@ function operate (event) {
 
     }
 
+    // Update stack display
     dispSTack ();
 
     inpSTack = [];
@@ -273,24 +328,55 @@ function operate (event) {
 
 function clearStack () {
 
+    // Clear number input stack
     inpSTack = [];
     inpSTack[0] = "";
 
+    // Clear calculator stack
     calSTack = [];
     calSTack[0] = "";
+    calSTack.pop();
 
-    output.innerHTML = 0;
-
+    // Clear stack displays
     xStackDisp.innerHTML = " ";
     yStackDisp.innerHTML = " ";
     zStackDisp.innerHTML = " ";
     wStackDisp.innerHTML = " ";
 
+    // Check if the shoft button is on
+
+    if (shiftFlag == "On") {
+
+        // Clear calculator display
+        output.innerHTML = " ";
+
+        // Switch off function displays
+        funcDisp1.style.display = "none";
+        funcDisp2.style.display = "none";
+
+        // Reset shift flag
+        shiftFlag = "Off";
+
+    } else {
+
+        // Reset calculator display to zero
+        output.innerHTML = 0;
+
+        // Switch off function displays
+        funcDisp1.style.display = "flex";
+        funcDisp2.style.display = "flex";
+
+    }
+    
+
 }
 
-// Event Listeners
 
+// Initiate calculator
 initCalc ();
+
+// Event Listeners
+modeBtn.addEventListener("click",initCalc);
 
 numKeyBtn.addEventListener("click",captUserInput);
 
@@ -299,3 +385,5 @@ enterBtn.addEventListener("click",pushStack);
 operandTwoKeyBtn.addEventListener("click",operate);
 
 clearBtn.addEventListener("click",clearStack);
+
+shiftBtn.addEventListener("click", ()=>{shiftFlag = "On";});
